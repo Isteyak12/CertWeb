b4436c1eaaa36950885ffa23bb0284263185df20
isteyak <isteyakislam12@gmail.com>
2024-06-24 18:55:33 -0400

    pdf update pending

--- Nodemailer/sendEmail.js
+++ Nodemailer/sendEmail.js
@@ -9,8 +9,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: "isteyakislam12@gmail.com",
-    pass: "pdjn flxu ghzt ngfa",
+    user: "testqqq808@gmail.com",
+    pass: "artz wqxy yqdt slcu",
   },
 });
 
@@ -21,7 +21,7 @@ const pdfFile = fs.readdirSync(currentDir).find(file => path.extname(file).toLow
 if (pdfFile) {
   // Create mail options with the PDF attachment
   const mailOptions = {
-    from: "isteyakislam12@gmail.com",
+    from: "testqqq808@gmail.com",
     to: "isteyakislam12@gmail.com",
     subject: "Hello from Nodemailer",
     text: "This is a test email sent using Nodemailer.",
